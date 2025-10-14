1513f4c3df3cd18a3c63b0199d13105314a0fc30
Aaaaaaaaaaaaaaaaaaaaron <aaron.lange@spontan.org>
2025-10-14 12:37:29 +0200

    Update to 60sec update time
    with millis

--- Sensor-read-test.cpp
+++ Sensor-read-test.cpp
@@ -10,13 +10,14 @@ board_build.partitions = huge_app.csv
 */
 
 
-
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
 Adafruit_BME280 bme; // I2C
 
+unsigned long lastRead = 0;
+const unsigned long interval = 60000; // 60 Sekunden
 /*
 3.3V → VCC
 GND → GND
@@ -24,19 +25,25 @@ GPIO21 → SDA
 GPIO22 → SCL
 */
 
+
 void setup() {
-  Serial.println("Hello World");
+  //Serial.println("Hello World");
   Serial.begin(115200);
+  delay(2000);  // 2 Sekunden warten, damit der Sensor hochfahren kann
   if (!bme.begin(0x76)) { // Adresse anpassen falls nötig
     Serial.println("Sensor nicht gefunden!");
     while (1);
   }
 }
 
+
 void loop() {
-  Serial.print("Temperatur: "); Serial.print(bme.readTemperature()); Serial.println(" °C");
-  Serial.print("Luftdruck: "); Serial.print(bme.readPressure()/100.0F); Serial.println(" hPa");
-  Serial.print("Luftfeuchte: "); Serial.print(bme.readHumidity()); Serial.println(" %");
-  Serial.println("----");
-  delay(2000);
+  if (millis() - lastRead >= interval) {
+    lastRead = millis();
+    float temp = bme.readTemperature();
+    float hum = bme.readHumidity();
+    float pres = bme.readPressure() / 100.0F;
+    Serial.printf("Temp: %.2f C  Hum: %.2f %%  Druck: %.2f hPa\n", temp, hum, pres);
+  }
 }
+
